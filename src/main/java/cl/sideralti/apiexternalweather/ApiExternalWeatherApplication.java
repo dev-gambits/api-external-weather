26ed156c3933bff447c23d3229172df0765ef6cc
dev-gambits <emmanuel.nieto.munoz@ciisa.cl>
2023-07-12 06:19:39 -0400

    first commit, clean architecture

---
+++ src/main/java/cl/sideralti/apiexternalweather/ApiExternalWeatherApplication.java
@@ -0,0 +1,13 @@
+package cl.sideralti.apiexternalweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExternalWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExternalWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/sideralti/apiexternalweather/ApiExternalAirlanesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.sideralti.apiexternalweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExternalAirlanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
